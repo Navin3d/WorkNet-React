b482c664a512b5141b9c6b97b7ea9fb6ca2bd09a
Navin3d <smnavin65@mail.com>
2023-04-30 21:05:26 +0530

    integrated backedn

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,55 +1,60 @@
 import React from 'react'
 import './Login.css'
 import axios from 'axios'
-
+import { useNavigate } from 'react-router-dom';
 function Login() {
-    const[username, setUserName] = React.useState();
-    const[password, setPassword] = React.useState();
+    const navigate = useNavigate();
+    const [username, setUserName] = React.useState();
+    const [password, setPassword] = React.useState();
     const handleSubmit = async (e) => {
         e.preventDefault();
         const res = await axios.post("http://localhost:6969/auth/login", { username, password });
         console.log(res.data);
-        localStorage.setItem("auth", new Boolean(res.data));
+        if (res.status == 200) {
+            localStorage.setItem("auth", new Boolean(res.data));
+            navigate("/");
+        }
     }
-  return (
-    <div><section>
-    <div class="login-box">
-        
-        <form onSubmit={handleSubmit}>
-            <h2>Login</h2>
-            <div class="input-box">
-                <span class="icon">
-                    <ion-icon name="mail"></ion-icon>
-                </span>
-                <input type="text" onChange={(e) => { setUserName(e.target.value)} } value={ username } required />
-                <label>Aadhar Yen</label>
-            </div> 
-            <div class="input-box">
-                <span class="icon"><ion-icon name="lock-closed"></ion-icon></span>
-                <input type="password" onChange={(e) => { setPassword(e.target.value)} } value={ password } required/>
-                <label>Password</label>
-            
-            </div>
-            <div class="conform_forget">
-                <label><input type="checkbox"/>
-                Confirm password</label>
-                <a href="#">Forget password</a>
-            </div>
-            
-            <button type="submit">Login</button>
-            <div class="sign up">
-                <p>Don't have an account?
-                    <a href="sign up.html">sign up</a>
-                </p>
-            </div>
-        </form>
-    </div>
-    </section>
+    return (
+        <div><section>
+            <div class="login-box">
 
-    {/* <script type="module" src="https://unpkg.com/ionicons@5.5.2/dist/ionicons/ionicons.esm.js"></script>
+                <form onSubmit={handleSubmit}>
+                    <h2>Login</h2>
+                    <div class="input-box">
+                        <label>Aadhar Yen</label>
+                        <span class="icon">
+                            <ion-icon name="mail"></ion-icon>
+                        </span>
+                        <input type="text" onChange={(e) => { setUserName(e.target.value) }} value={username} required />
+                    </div>
+
+                    <div class="input-box">
+                        <label>Password</label>
+                        <span class="icon"><ion-icon name="lock-closed"></ion-icon></span>
+                        <input type="password" onChange={(e) => { setPassword(e.target.value) }} value={password} required />
+                    </div>
+
+                    <div class="conform_forget">
+                        <label><input type="checkbox" />
+                            Confirm password</label>
+                        <a href="#">Forget password</a>
+                    </div>
+
+                    <button type="submit">Login</button>
+                    <div class="sign up">
+                        <p>Don't have an account?
+                            <a href="signup">sign up</a>
+                        </p>
+                    </div>
+                </form>
+            </div>
+        </section>
+
+            {/* <script type="module" src="https://unpkg.com/ionicons@5.5.2/dist/ionicons/ionicons.esm.js"></script>
     <script nomodule src="https://unpkg.com/ionicons@5.5.2/dist/ionicons/ionicons.js"></script> */}
-    </div>
-  )
+        </div>
+    )
 }
 
 export default Login
--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -1,76 +1,102 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './Register.css'
-
+import axios from 'axios';
+import { useNavigate } from 'react-router-dom';
+const INITIAL_USER = {
+    name: "",
+    location: "",
+    gender: "",
+    type_of_worker: "",
+    mobile_number: "",
+    aadhar_id: "",
+    password: ""
+};
 function Register() {
-  return (
-    <div>
-        <section>
-    <div class="register-box">
-        
-        <form action="">
-            <h2>REGISTER</h2>
-            <div class="reg-box">
-                <span class="icon">
-                    <ion-icon name="person"></ion-icon>
-                </span>
-                <input type="text" required/>
-                <label>Name</label>
-                     
-            </div> 
-            <div class="reg-box">
-                 <span class="icon">
-                    <ion-icon name="id-card"></ion-icon>
-                 </span>   
-                <input type="Aadhar" required/>
-                <label>Aadhar</label>
-                 
-            </div>
-           
-            <div class="reg-box">
-                <span class="icon">
-                    <ion-icon name="journal"></ion-icon>
-                </span>
-                <input type="text" required/>
-                <label>Type of work</label>
-                
-            </div> 
-            <div class="reg-box">
-                <span class="icon">
-                    <ion-icon name="pin"></ion-icon>
-                </span>
-                <input type="location" required/>
-                <label>Location</label>
-                
-            </div> 
-            
-            <div class="reg-box">
-                <span class="icon">
-                    <ion-icon name="transgender-outline"></ion-icon>
-                </span>
-                <input type="text" required/>
-                <label>Gender</label>
-                
-            </div> 
+    const navigate = useNavigate();
+    const [requestBody, setRequestBody] = useState(INITIAL_USER);
 
-           
+    const handleChange = (e) => {
+        const { name, value } = e.target;
+        setRequestBody((prev) => ({ ...prev, [name]: value }));
+    };
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+        const res = await axios.post("http://localhost:6969/auth/register", requestBody);
+        if (res.status == 200) {
+            navigate("/");
+        }
+    }
+    return (
+        <div>
+            <section>
+                <div class="register-box">
 
-            <div class="reg-box">
-                <span class="icon">
-                    <ion-icon name="call"></ion-icon>
-                </span>
-                <input type="password" required/>
-                <label>Phone number</label>
-            
-            </div>
-            
-            <button type="submit">Register</button>
-           {/* <div class="popup" id="popup">
+                    <form onSubmit={handleSubmit}>
+                        <h2>REGISTER</h2>
+                        <div class="reg-box">
+                            <span class="icon">
+                                <ion-icon name="person"></ion-icon>
+                            </span>
+                            <input type="text" name="name" onChange={handleChange} value={requestBody.name} required />
+                            <label>Name</label>
+
+                        </div>
+                        <div class="reg-box">
+                            <span class="icon">
+                                <ion-icon name="id-card"></ion-icon>
+                            </span>
+                            <input type="Aadhar" name="aadhar_id" onChange={handleChange} value={requestBody.aadhar_id} required />
+                            <label>Aadhar</label>
+                        </div>
+
+                        <div class="reg-box">
+                            <span class="icon">
+                                <ion-icon name="call"></ion-icon>
+                            </span>
+                            <input type="password" name="password" onChange={handleChange} value={requestBody.password} required />
+                            <label>Password</label>
+                        </div>
+
+                        <div class="reg-box">
+                            <span class="icon">
+                                <ion-icon name="journal"></ion-icon>
+                            </span>
+                            <input type="text" name="type_of_worker" onChange={handleChange} value={requestBody.type_of_worker} required />
+                            <label>Type of work</label>
+                        </div>
+
+                        <div class="reg-box">
+                            <span class="icon">
+                                <ion-icon name="pin"></ion-icon>
+                            </span>
+                            <input type="location" name="location" onChange={handleChange} value={requestBody.location} required />
+                            <label>Location</label>
+                        </div>
+
+                        <div class="reg-box">
+                            <span class="icon">
+                                <ion-icon name="transgender-outline"></ion-icon>
+                            </span>
+                            <input type="text" name="gender" onChange={handleChange} value={requestBody.gender} required />
+                            <label>Gender</label>
+                        </div>
+
+                        <div class="reg-box">
+                            <span class="icon">
+                                <ion-icon name="call"></ion-icon>
+                            </span>
+                            <input type="password" name="mobile_number" onChange={handleChange} value={requestBody.mobile_number} required />
+                            <label>Phone number</label>
+                        </div>
+
+                        <button type="submit">Register</button>
+                        {/* <div class="popup" id="popup">
                 <img src="404-tick.png"/>
                 <h2>Thank You</h2>
                 <p>Your details has been successfully submitted.thanks!</p>
                 <button type="button"  onclick="closepopup()">OK</button>
             </div> */}
-            {/* <script>
+                        {/* <script>
                 let popup=document.getElementById("popup");
                 function openpopup(){
                     popup.classList.add("open-popup");
@@ -79,19 +105,19 @@ function Register() {
                     popup.classList.remove("close-popup");
                 }
             </script> */}
-            <div class="register-link">
-                <p>Already have an account?
-                    <a href="/login">Login</a>
-                </p>
-            </div>
-        </form>
-    </div>
-    </section>
-    
-    {/* <script type="module" src="https://unpkg.com/ionicons@5.5.2/dist/ionicons/ionicons.esm.js"></script>
+                        <div class="register-link">
+                            <p>Already have an account?
+                                <a href="/login">Login</a>
+                            </p>
+                        </div>
+                    </form>
+                </div>
+            </section>
+
+            {/* <script type="module" src="https://unpkg.com/ionicons@5.5.2/dist/ionicons/ionicons.esm.js"></script>
     <script nomodule src="https://unpkg.com/ionicons@5.5.2/dist/ionicons/ionicons.js"></script></div> */}
-    </div>
-  )
+        </div>
+    )
 }
 
 export default Register
